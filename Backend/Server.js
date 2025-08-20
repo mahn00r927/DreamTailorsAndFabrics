ee472b44e9060461cfe5e238cd8c529f2cb87542
Mahnoor Rajput <mahnoor.rajput11927@gmail.com>
2025-08-20 20:54:25 +0500

    Some Styling of Pages

---
+++ Backend/Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');  
+
+const app = express();  
+const PORT = 5000;
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}   
+);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+    }   
+);          
